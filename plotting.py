4ac7acdbf507269b43a5984203e1bb20609b6fbe
seancraven <seanpietercraven@gmail.com>
2023-03-29 10:56:38 +0100

    cleaning up

--- plotting.py
+++ plotting.py
@@ -13,7 +13,8 @@ from src.pet_3.data import PetsDataFetcher
 if __name__ == "__main__":
     data = PetsDataFetcher("src/pet_3/").get_test_data()
     dmt_models_list = os.listdir("./models/dmt")
-    baseline_models_list = os.listdir("./models/baseline")
+    baseline_models_list = os.listdir("./models/baselines")
+    # Each baseline model has five copies
     baseline_models_list = [
         os.path.join("models", "baseline", f_name) for f_name in baseline_models_list
     ]
@@ -27,27 +28,6 @@ if __name__ == "__main__":
         baseline_models_list, evaluate_IoU, data
     )
     dmt_losses, dmt_models_list = evaluate_models(dmt_models_list, evaluate_IoU, data)
-    joint_list_names = []
-    joint_list_losses = []
-    for dmtl, dmtn, bl, bn in zip(
-        dmt_losses, dmt_models_list, baseline_losses, baseline_models_list
-    ):
-        joint_list_names.append(dmtn)
-        joint_list_names.append(bn)
-        joint_list_losses.append(dmtl)
-        joint_list_losses.append(bl)
-    print(joint_list_names)
-    print(joint_list_losses)
-
-    models_matshow_best_worst_img(
-        dmt_models_list, watched_evaluate_IoU, data, files_save_path="images/dmt"
-    )
-    models_matshow_best_worst_img(
-        baseline_models_list,
-        watched_evaluate_IoU,
-        data,
-        files_save_path="images/baseline",
-    )
-    models_bar(baseline_models_list, baseline_losses, "IoU")
-    models_bar(dmt_models_list, dmt_losses, "IoU")
-    models_bar(joint_list_names, joint_list_losses, "IoU")
+    list_loss = [sum(dmt_losses[i : i + 5]) / 5 for i in range(0, 25, 5)]
+    list_name = [dmt_models_list[i] for i in range(0, 25, 5)]
+    models_bar(list_name, list_loss, "IoU")
