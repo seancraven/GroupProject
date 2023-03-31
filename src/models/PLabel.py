bce2d52ee137de4692b8975432cbe606826f592b
Max Norman <maxnorman@eduroam-int-dhcp-97-17-30.ucl.ac.uk>
2023-03-31 18:15:40 +0100

    finished PLabel

--- src/models/PLabel.py
+++ src/models/PLabel.py
@@ -8,6 +8,10 @@ from src.utils.evaluation import evaluate_IoU
 from src.utils.training import PreTrainer, FineTuner as FT
 from functools import partial
 
+import time
+
+import wandb
+
 
 class PLabel(nn.Module):
     """Psuedo-Label"""
@@ -79,6 +83,7 @@ class PLabel(nn.Module):
         pseudolabels = torch.argmax(confidences, dim=-1)
         return pseudolabels
     
+    @staticmethod
     def compute_loss(
         self,
         confidences: torch.Tensor,
@@ -128,8 +133,11 @@ class PLabel(nn.Module):
         total_batches = min(len(self.labeled_loader), len(self.unlabeled_loader))
 
         for epoch in range(num_epochs):
-            epoch_loss = 0.0
+
+            epoch_labeled_loss = 0.0
+            epoch_unlabeled_loss = 0.0
             
+            tic = time.time()
             for t, (unlabeled, (labeled, labels)) in enumerate(
                 zip(self.unlabeled_loader, self.labeled_loader)
             ):
@@ -144,170 +152,86 @@ class PLabel(nn.Module):
                 # predict on unlabeled data
                 conf_unlabeled = self.model(unlabeled)
                 pseudolabels = self.compute_pseudolabels(conf_unlabeled)
-                epoch_loss += (1/len(self.labeled_loader))*self.compute_loss(conf_labeled, labels).sum() 
-                epoch_loss += (1/len(self.unlabeled_loader))*self.compute_loss(conf_unlabeled, pseudolabels).sum()
-
-                
-
-
-
-   def _train_from_teacher( # re-writter
-        self,
-        alpha: float,
-        num_epochs: int,
-        teacher: nn.Module,
-        student: nn.Module,
-        student_name: str = "Student",
-    ) -> None:
-        """
-        TODO docstring
-        """
-        teacher.eval()
-        student.train()
-
-        # Create the optimizer and scheduler as specified in the paper
-        fine_tuner = FT(student, num_epochs, self.labeled_loader, self.unlabeled_loader)
-        opt, scheduler = fine_tuner.optimizer, fine_tuner.scheduler
-
-        total_batches = min(len(self.labeled_loader), len(self.unlabeled_loader))
-        def _dynamic_gamma(gamma: float, t: int) -> float:
-            return gamma
-            total_train_steps = num_epochs * total_batches
-            return gamma * math.exp(5*(1 - (t / total_train_steps))**2)
-
-        for epoch in range(num_epochs):
-            epoch_dynamic_loss = 0.0
-            epoch_standard_loss = 0.0
-
-            tic = time.time()
-            for t, (unlabeled, (labeled, labels)) in enumerate(
-                zip(self.unlabeled_loader, self.labeled_loader)
-            ):
-                unlabeled, labeled, labels = map(
-                    lambda x: x.to(self.device), (unlabeled, labeled, labels)
-                )
-
-                opt.zero_grad()
-                # Compute the dynamic loss
-                teacher_confidences = teacher(unlabeled)
-                pseudolabels, mask = self.compute_pseudolabels(
-                    teacher_confidences, alpha
-                )
-                student_confidences = student(unlabeled)
-                with torch.no_grad():
-                    # Pretty sure this needs no_grad, because otherwise the model
-                    # seems to just learn to disagree with the teacher with high
-                    # confidence in order to minimise the weights
-                    weights = self.compute_weights(
-                        pseudolabels,
-                        mask,
-                        teacher_confidences,
-                        student_confidences,
-                        gamma_1=_dynamic_gamma(
-                            self.gamma_1_max,
-                            epoch * total_batches + t
-                        ),
-                        gamma_2=_dynamic_gamma(
-                            self.gamma_2_max,
-                            epoch * total_batches + t
-                        )
-                    )
-                # These lines do a sanity check
-                student_labels = torch.argmax(student_confidences, dim=-1)
-                # DMT.sanity_check(
-                #     unlabeled,
-                #     pseudolabels,
-                #     student_labels,
-                #     mask,
-                #     weights,
-                #     0,
-                #     "test1.png",
-                # )
-                # DMT.sanity_check(
-                #     unlabeled,
-                #     pseudolabels,
-                #     student_labels,
-                #     mask,
-                #     weights,
-                #     1,
-                #     "test2.png",
-                # )
-
-                dynamic_loss = self.compute_dynamic_loss(
-                    student_confidences, pseudolabels, weights
-                )
-                # Compute the standard loss
-                student_predictions = student(labeled)
-                standard_loss = self.compute_standard_loss(student_predictions, labels)
-                # Total loss and update
-                total_loss = dynamic_loss +  standard_loss
+                labeled_loss += self.compute_loss(conf_labeled, labels).sum() 
+                unlabeled_loss += self.compute_loss(conf_unlabeled, pseudolabels).sum()
+                total_loss = labeled_loss + unlabeled_loss
                 total_loss.backward()
                 opt.step()
 
-                epoch_dynamic_loss += dynamic_loss
-                epoch_standard_loss += standard_loss
+                # update epoch losses
+                epoch_labeled_loss += labeled_loss.item()
+                epoch_unlabeled_loss += unlabeled_loss.item()
+            
             scheduler.step()
             toc = time.time()
 
             # Bookkeeping
-            epoch_mean_dynamic_loss = epoch_dynamic_loss / (t + 1)
-            epoch_mean_standard_loss = epoch_standard_loss / (t + 1)
-            student_train_accuracy = evaluate_IoU(
-                student, self.labeled_loader, self.device
-            )
-            student_val_accuracy = evaluate_IoU(
-                student, self.validation_loader, self.device
-            )
+            epoch_mean_labeled_loss = epoch_labeled_loss / (t + 1)
+            epoch_mean_unlabeled_loss = epoch_unlabeled_loss / (t + 1)
+            epoch_mean_loss = epoch_mean_labeled_loss + epoch_mean_unlabeled_loss
 
-            if student is self.model_a:
-                if student_val_accuracy >= self.best_model_a_IoU:
-                    self.best_model_a_IoU = student_val_accuracy
-                    self.best_model_a_parameters = copy.deepcopy(student.state_dict())
-            elif student is self.model_b:
-                if student_val_accuracy >= self.best_model_b_IoU:
-                    self.best_model_b_IoU = student_val_accuracy
-                    self.best_model_b_parameters = copy.deepcopy(student.state_dict())
+            # I0U
+            train_accuracy = evaluate_IoU(
+                self.model, self.labeled_loader, self.device
+            )
+            val_accuracy = evaluate_IoU(
+                self.model, self.validation_loader, self.device
+            )
 
             # Everything below here is just logging
-            debug_msg = "Epoch {}/{} of percentile {} completed in {:2f} secs."
-            debug_msg_args = (epoch + 1, num_epochs, alpha, toc - tic)
-            self.debug(debug_msg.format(*debug_msg_args))
             self.wandb_log(
                 {
                     "Epoch time": toc - tic,
-                    "Epoch mean dynamic loss": epoch_mean_dynamic_loss,
-                    "Epoch mean standard loss": epoch_mean_standard_loss,
+                    "Epoch mean labeled loss": epoch_mean_labeled_loss,
+                    "Epoch mean unlabeled loss": epoch_mean_unlabeled_loss,
+                    "Epoch mean loss": epoch_mean_loss,
                 }
             )
             self.wandb_log_named(
                 {
-                    "Train IoU": student_train_accuracy,
-                    "Validation IoU": student_val_accuracy,
+                    "Train IoU": train_accuracy,
+                    "Validation IoU": val_accuracy,
                 },
-                student_name,
-            )
-            self.wandb_log_named(
-                {"Best validation IoU": self.best_model_a_IoU}, "Model A"
-            )
-            self.wandb_log_named(
-                {"Best validation IoU": self.best_model_b_IoU}, "Model B"
+                'dick',
             )
             if self.baseline is not None:
                 self.wandb_log_named(
                     {"Best validation IoU": self.baseline_IoU}, "Baseline"
                 )
+    @staticmethod
+    def wandb_init(
+        percentiles,
+        num_epochs,
+        batch_size,
+        label_ratio,
+    ):
+        try:
+            wandb.init(
+                project="DMT model",
+                config={
+                    "Percentiles": percentiles,
+                    "Number of epochs": num_epochs,
+                    "Batch size": batch_size,
+                    "Label ratio": label_ratio,
+                },
+            )
+        except:
+            pass
 
+    def save_model(self, filename: str) -> None:
+        """Save the model to the filename specified"""
+        if self.model is None:
+            raise ValueError("Model has not been trained yet")
+        torch.save(self.model.state_dict(), filename)
 
-        # TODO: maybe Load best parameters each model found on the validation set?
-        loader = DataLoader(
-            ConcatDataset(
-                [self.labeled_loader.dataset, self.validation_loader.dataset]
-            ),
-            batch_size=self.max_batch_size,
-            shuffle=True,
-        )
+    def save_baseline(self, filename: str) -> None:
+        """Save the baseline model to the filename specified"""
+        if not self.baseline:
+            raise ValueError("No baseline model was specified")
+        torch.save(self.baseline.state_dict(), filename)
 
-        self.best_model = max(
-            (self.model_a, self.model_b),
-            key=lambda model: evaluate_IoU(model, loader, self.device),
-        )
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward pass through the model"""
+        if self.model is None:
+            raise ValueError("Model has not been trained yet")
+        return self.model(x)
