33db4b4384cd7e237e6372b1539fb281c9944464
seancraven <seanpietercraven@gmail.com>
2023-04-06 11:51:29 +0100

    File docstring and some more detailed docstrings

--- src/utils/datasets.py
+++ src/utils/datasets.py
@@ -1,3 +1,7 @@
+"""
+Module to handel the logic around splitting or loading 
+datasets.
+"""
 import math
 
 from torch.utils.data import Dataset, Subset
@@ -10,6 +14,18 @@ def balanced_minibatch_sizes(
     """
     Computes batch sizes for the labeled and unlabeled datasets such that they
     each have as close to the same number of minibatches as possible.
+
+    Ensures that the whole unlabeled dataset is used once, when
+    there is more unlabeled data than labeled data.
+
+    Args:
+        labeled_dataset: The labeled dataset.
+        unlabeled_dataset: The unlabeled dataset.
+        max_batch_size: The maximum batch size.
+
+    Returns:
+        The batch sizes for the labeled and unlabeled datasets.
+
     """
     if len(labeled_dataset) >= len(unlabeled_dataset):
         labeled_batch_size = max_batch_size
@@ -29,10 +45,16 @@ def difference_maximized_sampling(
 ) -> Tuple[Dataset, Dataset]:
     """
     Returns two subsets of the dataset such that the overlap between them is minimized.
+    Overlap is defined as the number of elements that are in both subsets.
+
+    Args:
+        dataset: The dataset to split.
+        proportion: The proportion of the dataset to put in the first subset.
+
+    Returns:
+        Two subsets of the dataset.
     """
     subset_1 = Subset(dataset, range(int(len(dataset) * proportion)))
-    subset_2 = Subset(
-        dataset, range(int(1 - proportion) * len(dataset), len(dataset))
-    )
+    subset_2 = Subset(dataset, range(int(1 - proportion) * len(dataset), len(dataset)))
 
     return subset_1, subset_2
