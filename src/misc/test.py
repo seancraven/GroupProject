2d20f96adb81e03b7511b361bf2419725f7570d2
seancraven <seanpietercraven@gmail.com>
2023-04-06 16:07:30 +0100

    final cleans

--- src/misc/test.py
+++
@@ -1,8 +0,0 @@
-from src.testing.model_testing_utils import LoadedModel
-from src.pet_3.deprocated_data import Pets
-
-if __name__ == "__main__":
-    #dave = LoadedModel("models/u_net_supervised/Mean Squared Error Loss_20.pt")
-    pass
-    test_data = Pets("./src/pet_3/", split="test", binary_labels=True)
-    print(test_data.images)
